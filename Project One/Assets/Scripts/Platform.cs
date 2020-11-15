1fe77a002dc212a865226d6fe22de076159e915b
LukeWalker099 <73969853+LukeWalker099@users.noreply.github.com>
2020-11-15 12:45:09 +0000

    Added to Tutorial

---
+++ Project One/Assets/Scripts/Platform.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+	[SerializeField]
+	private bool moveRight;
+	public Rigidbody2D rb;
+	public float movSpeed;
+
+	private void Update()
+	
+	{
+		if (moveRight)
+		
+		{
+			transform.Translate(Vector3.left * movSpeed * Time.deltaTime);
+		}
+		else
+		{
+			transform.Translate(Vector3.right * movSpeed * Time.deltaTime);
+		}
+		
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+
+	{
+		if (other.gameObject.CompareTag("Turn"))
+		{
+			moveRight = false;
+		}
+		else
+		{
+			moveRight = true;
+		}
+	}
+
+}
+
